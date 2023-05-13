0219519ae57482907613655f41e33a35e56cb191
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-13 14:20:49 +0530

    feat: random gradient

--- src/gradient.rs
+++ src/gradient.rs
@@ -1,4 +1,7 @@
 use clap::Args;
+use owo_colors::OwoColorize;
+use palette::{rgb::Rgb, Gradient, LinSrgb};
+use rand::Rng;
 
 #[derive(Args, Debug)]
 pub struct GradientOptions {
@@ -23,5 +26,47 @@ pub fn generate(
     if color.len() > 0 {
     } else {
         let mut rng = rand::thread_rng();
+
+        let gradient = Gradient::new(vec![
+            LinSrgb::new(
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+            ),
+            LinSrgb::new(
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+            ),
+            LinSrgb::new(
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+                rng.gen_range(0.0..1.0),
+            ),
+        ]);
+
+        let taken_colors: Vec<_> = gradient
+            .take(*num_step)
+            .map(
+                |Rgb {
+                     red,
+                     green,
+                     blue,
+                     standard: _,
+                 }| {
+                    owo_colors::Rgb(
+                        (red * 255.0) as u8,
+                        (green * 255.0) as u8,
+                        (blue * 255.0) as u8,
+                    )
+                },
+            )
+            .collect();
+
+        for color in taken_colors.iter() {
+            let debug_str = "    ";
+
+            print!("{}", debug_str.on_color(*color))
+        }
     }
 }
