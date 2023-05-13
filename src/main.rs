3745d0da2de6f05795e64602966dcb50d0d881a9
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-13 22:32:32 +0530

    return miette Result in main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use clap::Parser;
 use color_gen::{gradient, Cli, Commands};
+use miette::{Context, Result};
 use owo_colors::OwoColorize;
 use rand::Rng;
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
         Commands::Gradient(grad_ops) => {
-            gradient::generate(grad_ops);
+            gradient::generate(grad_ops).wrap_err("While generating gradient")
         }
         Commands::Random => {
             let mut rng = rand::thread_rng();
@@ -27,7 +28,8 @@ fn main() {
                 color.0,
                 color.1,
                 color.2
-            )
+            );
+            Ok(())
         }
     }
 }
