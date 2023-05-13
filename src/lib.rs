7e48254ddb78be8d0714c1436c55e0837df769ad
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-13 22:22:59 +0530

    add custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use miette::Diagnostic;
+use thiserror::Error;
+
 pub mod gradient;
 
 #[derive(Parser)]
@@ -15,4 +18,24 @@ pub enum Commands {
     Random,
     /// Generate a gradient color
     Gradient(gradient::GradientOptions),
+}
+
+#[derive(Debug, Diagnostic, Error)]
+pub enum ColorGenerationError {
+    #[error(transparent)]
+    #[diagnostic(code(color_gen::io_error))]
+    IoError(#[from] std::io::Error),
+
+    #[error("number of colors and number of steps mismatch")]
+    #[diagnostic(code(color_gen::colors_and_steps_must_match))]
+    ColorsAndStepsMustMatch {
+        #[source_code]
+        input: String,
+        #[help]
+        advice: String,
+        #[label("colors")]
+        color_src: (usize, usize),
+        #[label("Steps")]
+        steps_src: (usize, usize),
+    },
 }
