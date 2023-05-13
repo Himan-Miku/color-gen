4b2572ebadc4d152208c9494562bd80d58bea705
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-13 12:14:15 +0530

    add random functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use color_gen::{Cli, Commands};
+use owo_colors::OwoColorize;
+use rand::Rng;
 
 fn main() {
     let cli = Cli::parse();
@@ -9,7 +11,23 @@ fn main() {
             println!("gradient match : {:?}", grad_ops)
         }
         Commands::Random => {
-            println!("Random match")
+            let mut rng = rand::thread_rng();
+
+            let color = owo_colors::Rgb(
+                rng.gen_range(0..255),
+                rng.gen_range(0..255),
+                rng.gen_range(0..255),
+            );
+
+            let debug_str = "    ";
+
+            print!(
+                "{} #{:x}{:x}{:x}",
+                debug_str.on_color(color),
+                color.0,
+                color.1,
+                color.2
+            )
         }
     }
 }
