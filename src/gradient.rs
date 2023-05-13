950b16a16f283bc85df177e06eb8df18b75938df
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-13 14:58:52 +0530

    feat: custom gradient 🪄

--- src/gradient.rs
+++ src/gradient.rs
@@ -1,3 +1,5 @@
+use std::{iter::zip, str::FromStr};
+
 use clap::Args;
 use owo_colors::OwoColorize;
 use palette::{rgb::Rgb, Gradient, LinSrgb};
@@ -26,6 +28,43 @@ pub fn generate(
     if colors.len() > 0 {
         if colors.len() != stops.len() {
             panic!("The number of colors and stops is not same:\nNumber of colors: {:?}\nNumber of stops: {:?}",colors, stops)
+        }
+
+        let color_list = zip(stops, colors)
+            .map(|(&stop, color)| {
+                (
+                    stop,
+                    LinSrgb::from_str(color)
+                        .expect("Hex code required")
+                        .into_format(),
+                )
+            })
+            .collect::<Vec<(f32, LinSrgb)>>();
+
+        let gradient = Gradient::with_domain(color_list);
+
+        let taken_colors: Vec<_> = gradient
+            .take(*num_step)
+            .map(
+                |Rgb {
+                     red,
+                     green,
+                     blue,
+                     standard: _,
+                 }| {
+                    owo_colors::Rgb(
+                        (red * 255.0) as u8,
+                        (green * 255.0) as u8,
+                        (blue * 255.0) as u8,
+                    )
+                },
+            )
+            .collect();
+
+        for color in taken_colors.iter() {
+            let debug_str = "    ";
+
+            print!("{}", debug_str.on_color(*color))
         }
     } else {
         let mut rng = rand::thread_rng();
